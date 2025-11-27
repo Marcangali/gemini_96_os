c61d6740038191cc9239c8b7647aa64f191cabf0
Marcangali <80262834+Marcangali@users.noreply.github.com>
2025-11-27 22:31:26 +0100

    Update icons.ts with local paths

---
+++ html/icons.ts
@@ -0,0 +1,84 @@
+export const iconsHTML = `
+
+      <!-- Desktop Icons -->
+      <div class="icon" data-app="myComputer">
+        <img
+          src="./icons/myComputer.png"
+          alt="My Computer"
+          style="width: 70px; height: 50px"
+        />
+        <span data-i18n-key="icon_myComputer">My Gemtop</span>
+      </div>
+      <div class="icon" data-app="chrome">
+        <img src="./icons/chrome.png" alt="Chrome" />
+        <span data-i18n-key="icon_chrome">Chrome</span>
+      </div>
+      <div class="icon" data-app="notepad">
+        <img src="./icons/notepad.png" alt="Notepad" />
+        <span data-i18n-key="icon_notepad">GemNotes</span>
+      </div>
+      <div class="icon" data-app="paint">
+        <img src="./icons/paint.png" alt="Paint" />
+        <span data-i18n-key="icon_paint">GemPaint</span>
+      </div>
+      <div class="icon" data-app="gemini">
+        <img
+          src="./icons/gemini.png"
+          alt="Gemini App"
+        />
+        <span data-i18n-key="icon_gemini">Gemini App</span>
+      </div>
+      <div class="icon" data-app="geminiStudio">
+        <img src="https://win98icons.alexmeub.com/icons/png/chm-2.png" alt="Gemini Studio" style="width: 48px; height: 48px;" />
+        <span data-i18n-key="icon_geminiStudio">Gemini Studio</span>
+      </div>
+      <div class="icon" data-app="minesweeper">
+        <img
+          src="./icons/minesweeper.png"
+          alt="Minesweeper"
+          style="width: 48px; height: 48px"
+        />
+        <span data-i18n-key="icon_minesweeper">GemSweeper</span>
+      </div>
+      <div class="icon" data-app="mediaPlayer">
+        <img
+          src="./icons/mediaPlayer.png"
+          alt="Media Player"
+          style="width: 48px; height: 48px"
+        />
+        <span data-i18n-key="icon_mediaPlayer">GemPlayer</span>
+      </div>
+      <div class="icon" data-app="flappyGemini">
+        <img src="./icons/flappyGemini.png" alt="Flappy Gemini" style="width: 48px; height: 48px;" />
+        <span data-i18n-key="icon_flappyGemini">Flappy Gemini</span>
+      </div>
+      <div class="icon" data-app="gemCraft">
+        <img src="./icons/gemCraft.png" alt="GemCraft" style="width: 48px; height: 48px;" />
+        <span data-i18n-key="icon_gemCraft">GemCraft</span>
+      </div>
+      <div class="icon" data-app="quizMania">
+          <img src="https://win98icons.alexmeub.com/icons/png/help_question_mark-0.png" alt="QuizMania" style="width: 48px; height: 48px;" />
+          <span data-i18n-key="icon_quizMania">QuizMania</span>
+      </div>
+       <div class="icon" data-app="settings">
+          <img src="https://win98icons.alexmeub.com/icons/png/settings_gear-1.png" alt="Settings" style="width: 48px; height: 48px;" />
+          <span data-i18n-key="icon_settings">Settings</span>
+      </div>
+      <div class="icon" data-app="twentyQuestions">
+        <img src="https://win98icons.alexmeub.com/icons/png/search_file-2.png" alt="20 Questions" style="width: 48px; height: 48px;" />
+        <span data-i18n-key="icon_twentyQuestions">20 Questions</span>
+      </div>
+      <div class="icon" data-app="paperclipClicker">
+        <img src="./icons/paperclipClicker.png" alt="Paperclip Mania" style="width: 48px; height: 48px;" />
+        <span data-i18n-key="icon_paperclipClicker">Paperclip Mania</span>
+      </div>
+      <div class="icon" data-app="appStore">
+        <img src="./icons/appStore.png" alt="Gemini Store" style="width: 48px; height: 48px;" />
+        <span data-i18n-key="icon_appStore">Gemini Store</span>
+      </div>
+      <div class="icon" data-app="iconMigrator">
+        <img src="https://win98icons.alexmeub.com/icons/png/paint_old-0.png" alt="Icon Migrator" style="width: 48px; height: 48px;" />
+        <span data-i18n-key="icon_iconMigrator">Icon Migrator</span>
+      </div>
+
+`;
